618f7c7155ca79112ad100ad7d18d07c275e7e69
WalaaHijazi1 <walaa.25.11@hotmail.com>
2025-07-08 12:51:38 +0300

    fixing alb error-1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -161,16 +161,6 @@ resource "aws_alb_listener" "http" {
     target_group_arn = aws_alb_target_group.app_tg.arn
   }
 }
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = aws_alb.app_LoadBalancer.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.app_tg.arn
-  }
-}
 
 # DB Subnet Group
 resource "aws_db_subnet_group" "django_db_subnet_group" {
